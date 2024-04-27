7564800f0ca443e0c0df4b20848ef7e72c799c24
sunshine-web-coder <webdev4gaming@gmail.com>
2024-04-27 20:40:42 +0100

    footer updated

--- src/components/accounts/footer/Footer.tsx
+++ src/components/accounts/footer/Footer.tsx
@@ -6,11 +6,11 @@ export default function Footer() {
   
   return (
     <div className="fixed p-4 sm:px-8 py-4 bottom-0 w-full bg-gray-300 left-0 right-0">
-      <div className="flex justify-between gap-3 w-full">
+      <div className="flex justify-between gap-4 w-full">
         {fMenuLink.map((link, i) => (
           <Link href={link.slug} key={i} className="flex flex-col w-full items-center justify-center">
             <span>{link.icon}</span>
-            <span>{link.label}</span>
+            <span className="text-[14px]">{link.label}</span>
           </Link>
         ))}
       </div>
